a48bd81e3be8951959182e6064a22baea7441b85
Mark Whatcott <whatcott1@gmail.com>
2020-04-01 22:06:44 -0500

    Rewrote script to include Episode class to manage individual episode attributes

--- pbkripper.py
+++ pbkripper.py
@@ -6,7 +6,7 @@ import requests
 from youtube_dl import YoutubeDL
 
 DOWNLOAD_ROOT = '.'
-DOWNLOAD_SUBTITLES = True
+DOWNLOAD_SUBTITLES = False
 SUBTITLE_TYPE = 'SRT'
 
 ydl = YoutubeDL({'outtmpl': '%(id)s.mp4'})
@@ -16,6 +16,84 @@ headers = requests.utils.default_headers()
 headers.update({'User-Agent': 'Mozilla/5.0 (Linux; Android 7.1.2; SM-G610M) '
                               'AppleWebKit/537.36 (KHTML, like Gecko) '
                               'Chrome/71.0.3578.99 Mobile Safari/537.36'})
+
+
+class Episode:
+    """All show episode attributes"""
+
+    def __init__(self, episode):
+        """
+        episode: downloaded json dictionary
+        """
+        self.mp4_url = episode['mp4']
+        self.id = episode['id']
+        self.slug = episode['program']['slug']
+        self.show_name = episode['program']['title'].strip()
+        self.episode_title = self.format_episode_title(title=episode['title'])
+        self.episode_number = self.format_episode_number(episode_num=episode['nola_episode'])
+        self.base_file_name = self.format_base_file_name()
+        self.full_file_path = os.path.join(DOWNLOAD_ROOT, self.show_name, self.base_file_name)
+        self.file_path_mp4 = f'{self.full_file_path}.mp4'
+        self.file_path_sub = f'{self.full_file_path}.{self.subtitle_type}'
+        self.subtitle_type = SUBTITLE_TYPE.lower()
+        self.subtitle_url = self.set_subtitles(closed_captions=episode['closedCaptions'])
+        self.file_dir = os.path.dirname(self.file_path_mp4)
+
+    def format_base_file_name(self):
+        if self.episode_number:
+            return f"{self.show_name}-{self.episode_number}-{self.episode_title}"
+        else:
+            return f"{self.show_name}-{self.episode_title}"
+
+    def create_output_files(self):
+        os.makedirs(self.file_dir, exist_ok=True)
+        if os.path.exists(self.file_path_mp4):
+            print(f"{self.episode_title} file exists, not downloading.")
+        else:
+            print(f"Downloading: {self.episode_number}")
+            # ydl.download function takes a list of URLs
+            video_url_as_list = [self.mp4_url]
+            ydl.download(video_url_as_list)
+            dl_info = ydl.extract_info(self.mp4_url, download=False)
+            orig_filename = dl_info['webpage_url_basename']
+            os.rename(orig_filename, self.file_path_mp4)
+            print(f'Downloaded file as {orig_filename}, renamed to {self.file_path_mp4}.')
+
+        if self.subtitle_url:
+            if not os.path.exists(self.file_path_sub):
+                caption_dl = requests.get(self.subtitle_url, headers=headers, stream=True)
+                caption_dl.raise_for_status()
+                print(f"Writing subtitles to: {self.file_path_sub}.")
+                with open(self.file_path_sub, 'wb') as f:
+                    for block in caption_dl.iter_content(1024):
+                        f.write(block)
+
+    @staticmethod
+    def format_episode_number(episode_num):
+        # A lot of episodes seem to not include a real number, so most of the time 'nola_episode'
+        # is just an abbreviation of the show title
+        if episode_num.isdigit():
+            # If episode number is like 301, split so it becomes S3E01
+            episode_number = episode_num[-2:]
+            season_number = episode_num.replace(episode_number, '')
+            if len(season_number) == 1:
+                season_number = f'0{season_number}'
+            return f'S{season_number}E{episode_number}'
+        else:
+            return ''
+
+    @staticmethod
+    def format_episode_title(title):
+        return title.replace('/', '-').replace("’", "").replace("'", "").replace("&", "and")
+
+    @staticmethod
+    def set_subtitles(closed_captions):
+        if DOWNLOAD_SUBTITLES:
+            for caption in closed_captions:
+                if caption['format'].lower() == SUBTITLE_TYPE.lower():
+                    return caption['URI']
+        else:
+            return ''
 
 
 def get_url_json(url):
@@ -28,7 +106,6 @@ def get_url_json(url):
 
 
 def get_shows():
-    # pbs_full_show_list_url = 'https://pbskids.org/pbsk/video/api/getShows/'
     pbs_full_show_list_url = 'https://content.services.pbskids.org/v2/kidspbsorg/home'
     json = get_url_json(pbs_full_show_list_url)
 
@@ -39,128 +116,65 @@ def get_shows():
     for key in show_dict_keys:
         for show_json in collections[key]['content']:
             combined_show_list.append(show_json)
-
     return combined_show_list
 
 
-def ask_which_show(show_list):
+def ask_which_show():
+    show_list = get_shows()
+    print(f"\nCurrent show list:\n===================")
+
     show_index = 1
     for x in show_list:
         print(f'[{show_index}]: {x["title"]}')
         show_index += 1
-    chosen_index = int(input(f'Select a show: [1 - {len(show_list)}]: '))
-    chosen_index = int(chosen_index) - 1
+    chosen_index = input(f'Select a show: [1 - {len(show_list)}], E=exit: ')
+    chosen_index = check_input(chosen_index, upper_limit=len(show_list))
     return show_list[chosen_index]['slug']
+
+
+def check_input(value, upper_limit):
+    if value.isdigit():
+        if 1 <= int(value) <= upper_limit:
+            return int(value) - 1
+        else:
+            sys.exit(f'Number entered was out of range, please restart.')
+    elif value.upper() == "A":  # Download all episodes of selected show
+        return value.upper()
+    elif value.upper() == "E":
+        sys.exit(f'Script exited.')
+    else:
+        sys.exit(f'Incorrect value entered, exiting.')
 
 
 def check_available_episodes(show_title):
     pbs_show_url = f'https://content.services.pbskids.org/v2/kidspbsorg/programs/{show_title}'
     json = get_url_json(pbs_show_url)
     episode_list = json['collections']['episodes']['content']
+    if not episode_list:
+        sys.exit(f'No episodes available for series: "{show_title}". Try another show next time.')
     return episode_list
 
 
-def ask_which_episode(episodes):
-    # episodes = check_available_episodes(show_title)
-    print(f"\n\nAvailable Episodes for {episodes[0]['program']['title']}:\n===================")
+def ask_which_episode(show_title):
+    episodes = check_available_episodes(show_title)
+    print(f"\nAvailable Episodes for {episodes[0]['program']['title']}:\n===================")
 
     index = 1
     for episode in episodes:
         print(f'[{index}]: {episode["title"]} - {episode["description"]}')
         index += 1
+    chosen_index = input(f'Which episode do you want? [1-{len(episodes)}], A=All, E=exit: ')
+    chosen_index = check_input(chosen_index, upper_limit=len(episodes))
 
-    chosen_index = input(f'Which episode do you want? [1 - {len(episodes)}], A=All: ')
-    if chosen_index.isdigit():
-        chosen_index = int(chosen_index) - 1
-    return chosen_index
-
-
-def get_video_info(video, subtitles=False):
-    info = dict()
-    info['mp4'] = video['mp4']
-    info['id'] = video['id']
-    info['slug'] = video['program']['slug']
-    info['show_name'] = video['program']['title'].strip()
-    info['episode_title'] = format_episode_title(video['title'])
-    info['episode_number'] = format_episode_number(video['nola_episode'])
-    if not info['episode_number']:
-        info['base_file_name'] = f"{info['show_name']}-{info['episode_title']}"
+    if chosen_index == "A":  # Download all episodes of selected show
+        for episode in episodes:
+            show_episode = Episode(episode)
+            show_episode.create_output_files()
     else:
-        info[
-            'base_file_name'] = f"{info['show_name']}-{info['episode_number']}-{info['episode_title']}"
-    info['video_file'] = os.path.join(DOWNLOAD_ROOT, info['show_name'], info['base_file_name'])
-    if subtitles:
-        for caption in video['closedCaptions']:
-            if caption['format'].lower() == SUBTITLE_TYPE.lower():
-                info['subtitle_url'] = caption['URI']
-    return info
-
-
-def format_episode_number(episode_num):
-    # A lot of episodes seem to not include a real number, so most of the time 'nola_episode'
-    # is just an abbreviation of the show title
-    if episode_num.isdigit():
-        # If episode number is like 301, split so it becomes S3E01
-        episode_number = episode_num[-2:]
-        season_number = episode_num.replace(episode_number, '')
-        if len(season_number) == 1:
-            season_number = f'0{season_number}'
-        return f'S{season_number}E{episode_number}'
-    else:
-        return ''
-
-
-def format_episode_title(episode_title):
-    return episode_title.replace('/', ' and ').replace("’", "").replace("'", "").replace("&", "and")
-
-
-def create_output_files(info_dict):
-    video_dir = os.path.dirname(info_dict['video_file'] + ".mp4")
-    print(f'video_dir: {video_dir}')
-    os.makedirs(video_dir, exist_ok=True)
-    mp4_file = f"{info_dict['video_file']}.mp4"
-    if os.path.exists(mp4_file):
-        print("Video file exists, not downloading.")
-        d = {'slug': info_dict['slug'], 'videofile': mp4_file}
-    else:
-        print(f"Writing to: {mp4_file}.")
-        # ydl.download function takes a list of URLs
-        video_url_as_list = [info_dict['mp4']]
-        ydl.download(video_url_as_list)
-        dl_info = ydl.extract_info(info_dict['mp4'], download=False)
-        orig_filename = dl_info['webpage_url_basename']
-        os.rename(orig_filename, mp4_file)
-        print(f'Downloaded file as {orig_filename}, renamed to {mp4_file}.')
-
-    if 'subtitle_url' in info_dict:
-        subtitle_extension = info_dict['subtitle_url'].split(".")[-1:]
-        print(subtitle_extension)
-        subtitle_extension = ''.join(subtitle_extension)
-        print(subtitle_extension)
-        subtitle_filename = info_dict['video_file'] + "." + subtitle_extension
-        if not os.path.exists(subtitle_filename):
-            caption_dl = requests.get(info_dict['subtitle_url'], headers=headers, stream=True)
-            caption_dl.raise_for_status()
-            print(f"Writing subtitles to: {subtitle_filename}.")
-            with open(subtitle_filename, 'wb') as f:
-                for block in caption_dl.iter_content(1024):
-                    f.write(block)
+        show_episode = Episode(episodes[chosen_index])
+        show_episode.create_output_files()
 
 
 if __name__ == '__main__':
-    shows = get_shows()
-    show = ask_which_show(shows)
-    available_episodes = check_available_episodes(show)
-    if not available_episodes:
-        sys.exit(f'No episodes available for series: "{show}". Try another show next time.')
-
-    # system('clear')
-    index_to_get = ask_which_episode(available_episodes)
-    if index_to_get.upper() == "A":  # Download all episodes of selected show
-        for item in available_episodes:
-            video_info = get_video_info(item, DOWNLOAD_SUBTITLES)
-            create_output_files(video_info)
-    else:  # Download only selected episode
-        index_to_get = int(index_to_get)
-        video_info = get_video_info(available_episodes[index_to_get], DOWNLOAD_SUBTITLES)
-        create_output_files(video_info)
+    show = ask_which_show()
+    ask_which_episode(show)
