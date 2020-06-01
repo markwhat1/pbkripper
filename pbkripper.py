b1a00b71fc1c84d3ab050757374d4ce5bb74f769
Mark Whatcott <whatcott1@gmail.com>
2020-05-31 20:40:49 -0500

    VS Code and black updates

--- pbkripper.py
+++ pbkripper.py
@@ -8,26 +8,36 @@ import requests
 from youtube_dl import YoutubeDL
 
 # DOWNLOAD_ROOT = 'C:/Users/markw/Downloads/pbs_tv_shows'
-DOWNLOAD_ROOT = '.'
+DOWNLOAD_ROOT = "."
 DOWNLOAD_SUBTITLES = False
-SUBTITLE_TYPE = 'SRT'
+SUBTITLE_TYPE = "SRT"
 
-ydl_opts = {'verbose': True, 'outtmpl': '%(title)s.%(ext)s',
-    'download_archive': 'download_archive.txt', 'ignoreerrors': True,  # ignore errors
-    'write_all_thumbnails': True, 'writesubtitles': True, 'allsubtitles': True, }
+ydl_opts = {
+    "verbose": True,
+    "outtmpl": "%(title)s.%(ext)s",
+    "download_archive": "download_archive.txt",
+    "ignoreerrors": True,  # ignore errors
+    "write_all_thumbnails": True,
+    "writesubtitles": True,
+    "allsubtitles": True,
+}
 
 ydl = YoutubeDL(ydl_opts)
 ydl.add_default_info_extractors()
 
-headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 7.1.2; SM-G610M) '
-                         'AppleWebKit/537.36 (KHTML, like Gecko) '
-                         'Chrome/71.0.3578.99 Mobile Safari/537.36'}
+headers = {
+    "User-Agent": "Mozilla/5.0 (Linux; Android 7.1.2; SM-G610M) "
+    "AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/71.0.3578.99 Mobile Safari/537.36"
+}
 
-translator = str.maketrans('', '', string.punctuation)
+translator = str.maketrans("", "", string.punctuation)
 
-logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s- %(message)s')
+logging.basicConfig(
+    level=logging.DEBUG, format=" %(asctime)s - %(levelname)s- %(message)s"
+)
 # logging.disable(logging.CRITICAL)
-logging.debug('Start of program')
+logging.debug("Start of program")
 
 
 class Episode:
@@ -37,18 +47,24 @@ class Episode:
         """
         episode: downloaded json dictionary
         """
-        self.mp4_url = episode['mp4']
-        self.subtitle_url = self.set_subtitles(closed_captions=episode['closedCaptions'])
+        self.mp4_url = episode["mp4"]
+        self.subtitle_url = self.set_subtitles(
+            closed_captions=episode["closedCaptions"]
+        )
         self.subtitle_type = SUBTITLE_TYPE.lower()
-        self.id = episode['id']
-        self.slug = episode['program']['slug']
-        self.show_name = episode['program']['title'].strip()
-        self.episode_title = self.format_episode_title(title=episode['title'])
-        self.episode_number = self.format_episode_number(episode_num=episode['nola_episode'])
+        self.id = episode["id"]
+        self.slug = episode["program"]["slug"]
+        self.show_name = episode["program"]["title"].strip()
+        self.episode_title = self.format_episode_title(title=episode["title"])
+        self.episode_number = self.format_episode_number(
+            episode_num=episode["nola_episode"]
+        )
         self.base_file_name = self.format_base_file_name()
-        self.full_file_path = os.path.join(DOWNLOAD_ROOT, self.show_name, self.base_file_name)
-        self.file_path_mp4 = f'{self.full_file_path}.mp4'
-        self.file_path_sub = f'{self.full_file_path}.{self.subtitle_type}'
+        self.full_file_path = os.path.join(
+            DOWNLOAD_ROOT, self.show_name, self.base_file_name
+        )
+        self.file_path_mp4 = f"{self.full_file_path}.mp4"
+        self.file_path_sub = f"{self.full_file_path}.{self.subtitle_type}"
         self.file_dir = os.path.dirname(self.file_path_mp4)
 
     def format_base_file_name(self):
@@ -67,15 +83,17 @@ class Episode:
             video_url_as_list = [self.mp4_url]
             ydl.download(video_url_as_list)
             dl_info = ydl.extract_info(self.mp4_url, download=False)
-            orig_filename = dl_info['webpage_url_basename']
+            orig_filename = dl_info["webpage_url_basename"]
             os.rename(orig_filename, self.file_path_mp4)
-            print(f'Downloaded file as {orig_filename}, renamed to {self.file_path_mp4}.')
+            print(
+                f"Downloaded file as {orig_filename}, renamed to {self.file_path_mp4}."
+            )
 
         if self.subtitle_url and not os.path.exists(self.file_path_sub):
             caption_dl = requests.get(self.subtitle_url, headers=headers, stream=True)
             caption_dl.raise_for_status()
             print(f"Writing subtitles to: {self.file_path_sub}.")
-            with open(self.file_path_sub, 'wb') as f:
+            with open(self.file_path_sub, "wb") as f:
                 for block in caption_dl.iter_content(1024):
                     f.write(block)
 
@@ -86,12 +104,12 @@ class Episode:
         if episode_num.isdigit():
             # If episode number is like 301, split so it becomes S3E01
             episode_number = episode_num[-2:]
-            season_number = episode_num.replace(episode_number, '')
+            season_number = episode_num.replace(episode_number, "")
             if len(season_number) == 1:
-                season_number = f'0{season_number}'
-            return f'S{season_number}E{episode_number}'
+                season_number = f"0{season_number}"
+            return f"S{season_number}E{episode_number}"
         else:
-            return ''
+            return ""
 
     @staticmethod
     def format_episode_title(title):
@@ -100,10 +118,10 @@ class Episode:
     def set_subtitles(self, closed_captions):
         if DOWNLOAD_SUBTITLES:
             for caption in closed_captions:
-                if caption['format'].lower() == self.subtitle_type:
-                    return caption['URI']
+                if caption["format"].lower() == self.subtitle_type:
+                    return caption["URI"]
         else:
-            return ''
+            return ""
 
 
 def get_url_json(url):
@@ -116,15 +134,19 @@ def get_url_json(url):
 
 
 def get_shows():
-    pbs_full_show_list_url = 'https://content.services.pbskids.org/v2/kidspbsorg/home'
+    pbs_full_show_list_url = "https://content.services.pbskids.org/v2/kidspbsorg/home"
     json = get_url_json(pbs_full_show_list_url)
 
     combined_show_list = []
-    collections = json['collections']
-    show_dict_keys = ['kids-show-spotlight', 'kids-programs-tier-1', 'kids-programs-tier-2',
-                      'kids-programs-tier-3']
+    collections = json["collections"]
+    show_dict_keys = [
+        "kids-show-spotlight",
+        "kids-programs-tier-1",
+        "kids-programs-tier-2",
+        "kids-programs-tier-3",
+    ]
     for key in show_dict_keys:
-        for show_json in collections[key]['content']:
+        for show_json in collections[key]["content"]:
             combined_show_list.append(show_json)
     return combined_show_list
 
@@ -140,10 +162,10 @@ def ask_which_show(index=None):
         for x in show_list:
             print(f'[{show_index}]: {x["title"]}')
             show_index += 1
-        chosen_index = input(f'Select a show: [1 - {len(show_list)}], E=exit: ')
+        chosen_index = input(f"Select a show: [1 - {len(show_list)}], E=exit: ")
 
     chosen_index = check_input(chosen_index, upper_limit=len(show_list))
-    return show_list[chosen_index]['slug']
+    return show_list[chosen_index]["slug"]
 
 
 def check_input(value, upper_limit):
@@ -151,21 +173,25 @@ def check_input(value, upper_limit):
         if 1 <= int(value) <= upper_limit:
             return int(value) - 1
         else:
-            sys.exit(f'Number entered was out of range, please restart.')
+            sys.exit(f"Number entered was out of range, please restart.")
     elif value.upper() == "A":  # Download all episodes of selected show
         return value.upper()
     elif value.upper() == "E":
-        sys.exit(f'Script exited.')
+        sys.exit(f"Script exited.")
     else:
-        sys.exit(f'Incorrect value entered, exiting.')
+        sys.exit(f"Incorrect value entered, exiting.")
 
 
 def check_available_episodes(show_title):
-    pbs_show_url = f'https://content.services.pbskids.org/v2/kidspbsorg/programs/{show_title}'
+    pbs_show_url = (
+        f"https://content.services.pbskids.org/v2/kidspbsorg/programs/{show_title}"
+    )
     json = get_url_json(pbs_show_url)
-    episode_list = json['collections']['episodes']['content']
+    episode_list = json["collections"]["episodes"]["content"]
     if not episode_list:
-        sys.exit(f'No episodes available for series: "{show_title}". Try another show next time.')
+        sys.exit(
+            f'No episodes available for series: "{show_title}". Try another show next time.'
+        )
     return episode_list
 
 
@@ -174,22 +200,26 @@ def ask_which_episode(show_title, download_all=False):
     if download_all:
         for count, episode in enumerate(episodes, start=1):
             show_episode = Episode(episode)
-            print(f'[Starting download {count} of {len(episodes)}.]')
+            print(f"[Starting download {count} of {len(episodes)}.]")
             show_episode.create_output_files()
     else:
-        print(f"\nAvailable Episodes for {episodes[0]['program']['title']}:\n===================")
+        print(
+            f"\nAvailable Episodes for {episodes[0]['program']['title']}:\n==================="
+        )
 
         x = 1
         for episode in episodes:
             print(f'[{x}]: {episode["title"]} - {episode["description"]}')
             x += 1
-        chosen_index = input(f'Which episode do you want? [1-{len(episodes)}], A=All, E=exit: ')
+        chosen_index = input(
+            f"Which episode do you want? [1-{len(episodes)}], A=All, E=exit: "
+        )
         chosen_index = check_input(chosen_index, upper_limit=len(episodes))
 
         if chosen_index == "A":  # Download all episodes of selected show
             for count, episode in enumerate(episodes, start=1):
                 show_episode = Episode(episode)
-                print(f'[Starting download {count} of {len(episodes)}.]')
+                print(f"[Starting download {count} of {len(episodes)}.]")
                 show_episode.create_output_files()
         else:
             show_episode = Episode(episodes[chosen_index])
@@ -200,13 +230,19 @@ def get_list_of_shows(index_list):
     pass
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # show = ask_which_show()
     # ask_which_episode(show)
 
-    show_index_list = [2, 3, 4, 19, ]
+    show_index_list = [
+        2,
+        3,
+        4,
+        19,
+    ]
     for show_index in show_index_list:
         show = ask_which_show(index=show_index)
         ask_which_episode(show, download_all=True)
 
-    print('Script has completed.')
+    print("Script has completed.")
+
