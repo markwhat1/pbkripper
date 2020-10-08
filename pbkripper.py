48304ed88d373a95d14ad5765a8fc60eebb41cfa
markwhat <whatcott1@gmail.com>
2020-10-07 23:58:15 -0500

    Added tvdb_api integration to automatically pull in episode numbers Refactored lots of the code

--- pbkripper.py
+++ pbkripper.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import logging
 import os
+import shutil
 import string
 import sys
-import shutil
 
 import requests
+import tvdb_api
 from youtube_dl import YoutubeDL
 
 # DOWNLOAD_ROOT = 'C:/Users/markw/Downloads/pbs_tv_shows'
@@ -17,6 +18,8 @@ SUBTITLE_TYPE = "SRT"
 # https://github.com/ytdl-org/youtube-dl/blob/718393c632df5106df92c60c650f52d86a9a3510/youtube_dl/YoutubeDL.py#L137-L312
 ydl_opts = {
     # "verbose": True,
+    "quiet": True,
+    "no_warnings": True,
     "outtmpl": "%(title)s.%(ext)s",
     "download_archive": "download_archive.txt",
     "ignoreerrors": True,  # ignore errors
@@ -35,11 +38,15 @@ ydl.add_default_info_extractors()
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Linux; Android 7.1.2; SM-G610M) "
-    "AppleWebKit/537.36 (KHTML, like Gecko) "
-    "Chrome/71.0.3578.99 Mobile Safari/537.36"
+                  "AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/71.0.3578.99 Mobile Safari/537.36"
 }
 
-translator = str.maketrans("", "", string.punctuation)
+PUNCT_TO_REMOVE = string.punctuation.replace("/", "")  # Keep dashes
+translator = str.maketrans("", "", PUNCT_TO_REMOVE)
+
+tvdb_key = '1DE5C9B35180B706'
+t = tvdb_api.Tvdb(apikey=tvdb_key)
 
 logging.basicConfig(
     level=logging.DEBUG, format=" %(asctime)s - %(levelname)s- %(message)s"
@@ -64,43 +71,39 @@ class Episode:
         self.slug = episode["program"]["slug"]
         self.show_name = episode["program"]["title"].strip()
         self.episode_title = self.format_episode_title(title=episode["title"])
-        self.episode_number = self.format_episode_number(
-            episode_num=episode["nola_episode"]
-        )
-        self.base_file_name = self.format_base_file_name()
+        self.episode_number = ''
         if DOWNLOAD_ROOT == ".":
             self.full_file_path = os.path.join(
-                os.getcwd(), self.show_name, self.base_file_name
+                os.getcwd(), self.show_name, self.episode_title
             )
         else:
             self.full_file_path = os.path.join(
-                DOWNLOAD_ROOT, self.show_name, self.base_file_name
+                DOWNLOAD_ROOT, self.show_name, self.episode_title
             )
         self.file_path_mp4 = f"{self.full_file_path}.mp4"
         self.file_path_sub = f"{self.full_file_path}.{self.subtitle_type}"
         self.file_dir = os.path.dirname(self.file_path_mp4)
-
-    def format_base_file_name(self):
-        if self.episode_number:
-            return f"{self.show_name}-{self.episode_number}-{self.episode_title}"
-        else:
-            return f"{self.show_name}--{self.episode_title}"
 
     def create_output_files(self):
         os.makedirs(self.file_dir, exist_ok=True)
         if os.path.exists(self.file_path_mp4):
             print(f"{self.episode_title} file exists, not downloading.")
         else:
-            print(f"Downloading: {self.episode_number}")
-            # ydl.download function takes a list of URLs
-            video_url_as_list = [self.mp4_url]
-            ydl.download(video_url_as_list)
-            dl_info = ydl.extract_info(self.mp4_url, download=False)
-            orig_filename = dl_info["webpage_url_basename"]
-            shutil.move(orig_filename, self.file_path_mp4)
-            print(
-                f"Downloaded file as {orig_filename}, renamed to {self.file_path_mp4}."
-            )
+            try:
+                print(f"Downloading: {self.episode_title}")
+                # ydl.download function takes a list of URLs
+                video_url_as_list = [self.mp4_url]
+                ydl.download(video_url_as_list)
+                dl_info = ydl.extract_info(self.mp4_url, download=False)
+                orig_filename = dl_info["webpage_url_basename"]
+                shutil.move(orig_filename, self.file_path_mp4)
+                print(
+                    f"Downloaded file as {orig_filename}, renamed to {self.file_path_mp4}."
+                )
+                return True
+            except FileNotFoundError:
+                return False
+                pass
 
         if self.subtitle_url and not os.path.exists(self.file_path_sub):
             caption_dl = requests.get(self.subtitle_url, headers=headers, stream=True)
@@ -110,21 +113,64 @@ class Episode:
                 for block in caption_dl.iter_content(1024):
                     f.write(block)
 
-    @staticmethod
-    def format_episode_number(episode_num):
-        # A lot of episodes seem to not include a real number, so most of the time 'nola_episode'
-        # is just an abbreviation of the show title
-        if episode_num.isdigit():
-            # If episode number is like 301, split so it becomes S3E01
-            season_number = int(episode_num[:-2])  # Everything before lat 2 characters
-            episode_number = int(episode_num[-2:])  # Last 2 characters
-            return f"S{season_number:02}E{episode_number:02}"
+    def format_episode_title(self, title):
+        title_translated = title.translate(translator)
+        if "/" in title:
+            titles = title.split('/')
         else:
-            return ""
+            titles = [title]
 
-    @staticmethod
-    def format_episode_title(title):
-        return title.translate(translator)
+        # Search tvdb for tv_show slug
+        show_tvdb = t[self.slug]
+        episode_numbers = list()
+        season_numbers = list()
+        for x in titles:
+            x_tvdb = show_tvdb.search(rf'{x}', key='episodeName')
+            if len(x_tvdb) == 1:
+                x_tvdb = x_tvdb[0]
+                season_numbers.append(x_tvdb['airedSeason'])
+                episode_numbers.append(x_tvdb['airedEpisodeNumber'])
+            elif len(x_tvdb) == 0:
+                x = x.split(' ')[-1]
+                x_tvdb = show_tvdb.search(rf'{x}', key='episodeName')
+                if len(x_tvdb) == 1:
+                    x_tvdb = x_tvdb[0]
+                    season_numbers.append(x_tvdb['airedSeason'])
+                    episode_numbers.append(x_tvdb['airedEpisodeNumber'])
+                else:
+                    print(f'No matching EPISODE results for: {x} in {self.show_name}')
+                    self.episode_number = 'S00E00'
+                    self.episode_title = f'{self.show_name} -{self.episode_number}- {title_translated}'
+                    return self.episode_title
+            else:
+                print(f'Too many EPISODE results for: {x} in {self.show_name}')
+                self.episode_number = 'S00E00'
+                self.episode_title = f'{self.show_name} -{self.episode_number}- {title_translated}'
+
+        self.set_episode_number(season_list=season_numbers, episode_list=episode_numbers)
+
+        # Translator removes all punctuation except '/' to allow for dual episode renaming
+        # Replacing the '/' with a '-' for dual episodes
+        title = title.translate(translator).replace('/', '-')
+
+        # Form complete title
+        self.episode_title = f'{self.show_name} - {self.episode_number} - {title}'
+        return self.episode_title
+
+    def set_episode_number(self, season_list, episode_list):
+        # Use list(set()) to remove duplicates
+        season_list = list(set(season_list))
+        season_number = season_list[0]
+        episode_list = list(set(episode_list))
+
+        # Combine episode numbers if necessary
+        if len(episode_list) == 2:
+            episode_numbers = f'E{episode_list[0]:02}-E{episode_list[1]:02}'
+        else:
+            episode_numbers = f'E{episode_list[0]:02}'
+
+        # Make full season/episode key
+        self.episode_number = f'S{season_number:02}{episode_numbers}'
 
     def set_subtitles(self, closed_captions):
         if DOWNLOAD_SUBTITLES:
@@ -162,17 +208,32 @@ def get_shows():
     return combined_show_list
 
 
-def ask_which_show(index=None):
+def get_show_slug_list():
     show_list = get_shows()
-    if index:
+    slug_list = list()
+    for x in show_list:
+        x_title = x['title']
+        x_slug = x['slug']
+        slug_list.append(f"{x_title} = '{x_slug}'")
+    sorted_slugs = sorted(slug_list, key=str.lower)
+    for count, slug in enumerate(sorted_slugs, start=1):
+        print(f'{count}: {slug}')
+
+
+def ask_which_show(index=None, show_name=None):
+    show_list = get_shows()
+    if show_name:
+        chosen_show = show_name
+        for x in show_list:
+            if x['title'] == chosen_show:
+                return show['slug']
+    elif index:
         chosen_index = index
     else:
         print(f"\nCurrent show list:\n===================")
 
-        show_index = 1
-        for x in show_list:
+        for show_index, x in enumerate(show_list, start=1):
             print(f'[{show_index}]: {x["title"]}')
-            show_index += 1
         chosen_index = input(f"Select a show: [1 - {len(show_list)}], E=exit: ")
 
     chosen_index = check_input(chosen_index, upper_limit=len(show_list))
@@ -184,7 +245,7 @@ def check_input(value, upper_limit):
         if 1 <= int(value) <= upper_limit:
             return int(value) - 1
         else:
-            sys.exit(f"Number entered was out of range, please restart.")
+            sys.exit("Number entered was out of range, please restart.")
     elif value.upper() == "A":  # Download all episodes of selected show
         return value.upper()
     elif value.upper() == "E":
@@ -211,48 +272,47 @@ def ask_which_episode(show_title, download_all=False):
     if download_all:
         for count, episode in enumerate(episodes, start=1):
             show_episode = Episode(episode)
-            print(f"[Starting download {count} of {len(episodes)}.]")
+            print(f"[Checking file {count} of {len(episodes)}.]")
             show_episode.create_output_files()
     else:
         print(
             f"\nAvailable Episodes for {episodes[0]['program']['title']}:\n==================="
         )
 
-        x = 1
-        for episode in episodes:
-            print(f'[{x}]: {episode["title"]}')
-            x += 1
+        for count, episode in enumerate(episodes, start=1):
+            print(f'[{count}]: {episode["title"]}')
         chosen_index = input(
             f"Which episode do you want? [1-{len(episodes)}], A=All, E=exit: "
         )
         chosen_index = check_input(chosen_index, upper_limit=len(episodes))
 
+        dl_count = 0
         if chosen_index == "A":  # Download all episodes of selected show
             for count, episode in enumerate(episodes, start=1):
                 show_episode = Episode(episode)
                 print(f"[Starting download {count} of {len(episodes)}.]")
-                show_episode.create_output_files()
+                dl_status = show_episode.create_output_files()
+                if dl_status:
+                    dl_count += 1
+            print(f"Downloaded {dl_count} new episodes of {show_title}.")
         else:
             show_episode = Episode(episodes[chosen_index])
             show_episode.create_output_files()
 
 
-def get_list_of_shows(index_list):
-    pass
-
-
 if __name__ == "__main__":
-    show = ask_which_show()
-    ask_which_episode(show)
+    # wanted_shows = 'slug' values
+    # See list of available slug values
+    # get_show_slug_list()
 
-    # show_index_list = [
-    #     2,
-    #     3,
-    #     4,
-    #     19,
-    # ]
-    # for show_index in show_index_list:
-    #     show = ask_which_show(index=show_index)
-    #     ask_which_episode(show, download_all=True)
+    # wanted_shows = ''
+    # wanted_shows = ['daniel-tigers-neighborhood', 'wild-kratts']
+    wanted_shows = ['wild-kratts']
+    if wanted_shows:
+        for item in wanted_shows:
+            ask_which_episode(item, download_all=True)
+    else:
+        show = ask_which_show()
+        ask_which_episode(show)
 
     print("Script has completed.")
